79890a8a1054b1e38b9a6f39adfb3f0aeb5b64e0
Dominic Swaine <dominic.swaine@gmail.com>
2019-09-05 02:33:16 +0100

    Add newsfeed redux logic code

--- actions/types.js
+++ actions/types.js
@@ -1,1 +1,6 @@
 export const DETERMINE_EMAIL_ASSOC_WITH_ACCOUNT = 'determine_email_assoc_with_account';
+export const NEWS_REQUESTED = 'news_requested';
+export const NEWS_RETREIVED = 'news_retrieved';
+export const NEWS_LOADED = 'news_loaded';
+export const CLEAR_NEWS = 'clear_news';
+export const NO_FURTHER_NEWS = 'no_further_news';
