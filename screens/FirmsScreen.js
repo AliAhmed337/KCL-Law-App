8a6a269ca6878ba437a40427e0ea710833b2d244
Dominic Swaine <dominic.swaine@gmail.com>
2019-09-05 15:35:05 +0100

    Correct LawFirms page title, remove logs

--- screens/FirmsScreen.js
+++ screens/FirmsScreen.js
@@ -2,10 +2,8 @@ import React from 'react';
 import {View, Button, Text, AsyncStorage, ScrollView, StyleSheet, Image} from 'react-native';
 import {StatusBar} from "../custom_components/StatusBar";
 import {HeaderComponent} from "../custom_components/HeaderComponent";
-console.log("b");
 import {FirmBar} from "../custom_components/feed_components/FirmBar";
 
-console.log("a");
 class FirmsScreen extends React.Component {
     static navigationOptions = {header: null};
 
@@ -15,7 +13,7 @@ class FirmsScreen extends React.Component {
                 <StatusBar/>
                 <ScrollView style={{backgroundColor: '#F5F5F5', flex: 1}}>
                     <View style={{paddingTop: 20, paddingBottom: 0, paddingLeft: 15, paddingRight: 15}}>
-                        <HeaderComponent pageName={'About'} />
+                        <HeaderComponent pageName={'Law Firms'} />
                     </View>
 
                     <FirmBar />
