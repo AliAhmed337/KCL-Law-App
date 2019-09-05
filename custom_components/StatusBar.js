2e094671dea59963c07d138d4cfcc972ec9ac50b
Dominic Swaine <dominic.swaine@gmail.com>
2019-09-05 02:34:44 +0100

    Update StatusBar design

--- custom_components/StatusBar.js
+++ custom_components/StatusBar.js
@@ -4,8 +4,10 @@ import Constants from "expo-constants";
 
 const s = StyleSheet.create({
     statusBar: {
-        backgroundColor: "#C2185B",
-        height: Constants.statusBarHeight,
+        backgroundColor: "#EEEEEE",
+        borderBottomWidth: 1,
+        borderBottomColor: "#e0e0e0",
+        height: Constants.statusBarHeight + 1,
     }
 });
 
